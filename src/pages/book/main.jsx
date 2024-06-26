d93fb233ffec9aca5a43c1edd5e197f2bee2dbbe
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-06-27 02:00:51 +0530

    eee

--- src/pages/book/main.jsx
+++ src/pages/book/main.jsx
@@ -13,27 +13,10 @@ function Book() {
             <Topbar />
             <Sidebar />
             <div className={styles.container}>
-                <div className={styles.book}>
-                    <div className={styles.cover-and-desc}>
-                        <div className={styles.cover}></div>
-                        <div className={styles.desc}></div>
-                    </div>
-                    <div className={styles.info}>
-                        <div className={styles.title}></div>
-                        <div className={styles.author}></div>
-                        <div className={styles.rating}></div>
-                        <div className={styles.price}></div>
-                        <div className={styles.buy}></div>
-                    </div>
-                </div>
-                <div className={styles.author}>
-                    <div className={styles.avatar}></div>
-                    <div className={styles.name}></div>
-                    <div className={styles.bio}></div>
-                </div>
+
             </div>
         </>
-    )
+    );
 }
 
 export default Book;
