98b2faf9704d0824c72276524d4004bca0e8e1f2
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-07-17 00:59:46 +0530

    added links

--- src/pages/categories/main.jsx
+++ src/pages/categories/main.jsx
@@ -28,79 +28,22 @@ function Category() {
             <Sidebar />
             <div className={styles.container}>
                 <div className={styles.main}>
-                    <Link to={`/books?category={poetry}`} className={styles.category}>
-                        <div>
+                    {categories.map((category, index) => (
+                        <Link
+                            key={index}
+                            to={`/books?category=${category.category}`}
+                            className={styles.category}
+                        >
                             <div className={styles.categoryImage}>
-                                <img src="src\pages\categories\poetry.jpeg" alt="Category" />
+                                <img
+                                    src={`src/pages/categories/${category.image}`}
+                                    alt={category.name}
+                                />
                             </div>
-                            <div className={styles.categoryTitle}>Poetry</div>
+                            <div className={styles.categoryTitle}>{category.name}</div>
                             <div className={styles.categoryNumber}>564 Items</div>
-                        </div>
-                    </Link>
-                        <div className={styles.category}>
-                            <div className={styles.categoryImage}>
-                                <img src="src\pages\categories\romance.jpeg" alt="Category" />
-                            </div>
-                            <div className={styles.categoryTitle}>Romance</div>
-                            <div className={styles.categoryNumber}>564 Items</div>
-                        </div>
-                        <div className={styles.category}>
-                            <div className={styles.categoryImage}>
-                                <img src="src\pages\categories\fiction.jpeg" alt="Category" />
-                            </div>
-                            <div className={styles.categoryTitle}>Fiction</div>
-                            <div className={styles.categoryNumber}>564 Items</div>
-                        </div>
-                        <div className={styles.category}>
-                            <div className={styles.categoryImage}>
-                                <img src="src\pages\categories\nonfiction.jpeg" alt="Category" />
-                            </div>
-                            <div className={styles.categoryTitle}>Non-fiction</div>
-                            <div className={styles.categoryNumber}>564 Items</div>
-                        </div>
-                        <div className={styles.category}>
-                            <div className={styles.categoryImage}>
-                                <img src="src\pages\categories\memoirs.jpeg" alt="Category" />
-                            </div>
-                            <div className={styles.categoryTitle}>Memoirs</div>
-                            <div className={styles.categoryNumber}>564 Items</div>
-                        </div>
-                        <div className={styles.category}>
-                            <div className={styles.categoryImage}>
-                                <img src="src\pages\categories\shortstories.jpeg" alt="Category" />
-                            </div>
-                            <div className={styles.categoryTitle}>Short Stories</div>
-                            <div className={styles.categoryNumber}>564 Items</div>
-                        </div>
-                        <div className={styles.category}>
-                            <div className={styles.categoryImage}>
-                                <img src="src\pages\categories\historical.jpeg" alt="Category" />
-                            </div>
-                            <div className={styles.categoryTitle}>Historical</div>
-                            <div className={styles.categoryNumber}>564 Items</div>
-                        </div>
-                        <div className={styles.category}>
-                            <div className={styles.categoryImage}>
-                                <img src="src\pages\categories\drama.jpeg" alt="Category" />
-                            </div>
-                            <div className={styles.categoryTitle}>Drama</div>
-                            <div className={styles.categoryNumber}>564 Items</div>
-                        </div>
-                        <div className={styles.category}>
-                            <div className={styles.categoryImage}>
-                                <img src="src\pages\categories\graphicnovels.jpeg" alt="Category" />
-                            </div>
-                            <div className={styles.categoryTitle}>Graphic Novels</div>
-                            <div className={styles.categoryNumber}>564 Items</div>
-                        </div>
-
-                        <div className={styles.category}>
-                            <div className={styles.categoryImage}>
-                                <img src="src\pages\categories\essays.jpeg" alt="Category" />
-                            </div>
-                            <div className={styles.categoryTitle}>Essays</div>
-                            <div className={styles.categoryNumber}>564 Items</div>
-                        </div>
+                        </Link>
+                    ))}
                 </div>
             </div>
         </>
